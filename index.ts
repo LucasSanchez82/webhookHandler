a48aeec6d901f9346702c295311c3ba2a6b5fc6a
LucasSanchez82 <08.lucas.sanchez@gmail.com>
2025-03-11 16:42:17 +0100

    refactor webhook handler to use async/await and return status based on updateManweb result

--- index.ts
+++ index.ts
@@ -8,17 +8,20 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.post("/manwebv2", (req: Request, res: Response) => {
+app.post("/manwebv2", async (req: Request, res: Response) => {
   const event = req.headers["x-github-event"];
   const payload = req.body;
 
   if (event === "push" && payload.ref === "refs/heads/prod") {
     console.log("Received a push event for the prod branch");
     Log.add("Received a push event for the prod branch");
-    updateManweb();
-  } else Log.add("Received an other event than the prod branch");
-
-  res.status(200).send("Webhook received");
+    const isSuccesful = await updateManweb();
+    res
+      .status(isSuccesful ? 200 : 500)
+      .send(isSuccesful ? "Update successful" : "Update failed");
+  }
+  Log.add("Received an other event than the prod branch");
+  res.status(200).send("Received an other event than the prod branch");
 });
 
 app.listen(port, () => {
